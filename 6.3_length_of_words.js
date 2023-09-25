4631abd9453c6e6c7f601736347aca9a3cf8fd0c
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-25 16:20:38 -0300

    Module od today 19.9.2023

---
+++ 6.3_length_of_words.js
@@ -0,0 +1,8 @@
+let arr =[`Hello World!`, `Heyyyy`, `Byeeeeee`, `Welcome!!!`, `Se ya later!`];
+let lengthOfArr = [];
+
+for (i=0; i<=arr.length-1; i++){
+    lengthOfArr.push(arr[i].length);
+}
+
+console.log(lengthOfArr)
