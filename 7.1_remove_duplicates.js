3077d43f18c0238a035af2552427a12ede2c00f0
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-25 17:26:54 -0300

    Module of today 19.9.2023

--- 7.1_remove_duplicates.js
+++ 7.1_remove_duplicates.js
@@ -1,9 +1,9 @@
-let arr = [3 ,4 ,4 ,3 ,6 ,3 ];
+let arr = [3 ,4 ,4 ,3 ,6 ,3 ,5 ,3 ,7 ,6 ,5 ,4 ,5];
 
 function noDuplicate() {
     let arrNoDuplicate=[];
-    for (let i=0; i<arr.length-1; i++){
-    if(arr[i]!==arr[i+1] && arr[i]!==arr[i+2])
+    for (let i=0; i<arr.length; i++){
+    if(arrNoDuplicate.indexOf(arr[i]) === -1)
         arrNoDuplicate.push(arr[i])
     }
     return arrNoDuplicate
