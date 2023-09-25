daf499085f34a91db787b161a374035d4e06a1b6
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-24 22:08:18 -0300

    Module of today 18.9.2023

---
+++ 1.2_more_on_primitive_data_types.js
@@ -0,0 +1,41 @@
+1. Which of the following is/are strings?
+
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+d) -43. Which of the following is/are booleans?
+a) true
+b) false
+4. What is the result of 80 + 71.2?
+a) 151.2
+5. What is the result of "80" + 71.2?
+c) "71.280"
+6. Does 100 + 30 produce a number or a string?
+a) number
+7. Does "100" + 30 produce a number or a string?
+a)number
+
+*///------Submit your answers under the
+question------//
+//create three different ways to declare variables
+1. let
+2. var
+3. const
+//declare a variable and reassign a value to it
+let a = 13;
+//create a variable and after that assign a string
+var hellow = 'He`s got it!'
+with its value being: He's got it!
+/*
+1. create a variable and assign a value on how
+much a restaurant bill is.
+let moneyCost = 
+2. create a variable and assign a value on how
+much tax they should pay.
+3. create a variable that will calculate the bill
+* tax and its output would be: Your total bill is
+<total bill> $.
+*/
+// Round the number 50.6 to its nearest integer
+//Create a variable that is undefined
---
+++ test.js
@@ -0,0 +1,4 @@
+let a = 71.2;
+let b = "80";
+
+console.log(a+b);
