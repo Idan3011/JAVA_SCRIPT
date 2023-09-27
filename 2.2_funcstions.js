db3ab1223edf5883aa2de9af5361e3b4b6977aab
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-27 14:52:31 -0300

    Module of today 20.9.2023

---
+++ 2.2_funcstions.js
@@ -0,0 +1,5 @@
+function tellFortune(jobTitle, location, partnerName, numberOfChildren ) {
+    console.log(`You will be a ${jobTitle} in ${location} married to ${partnerName} with ${numberOfChildren}`)
+}
+
+tellFortune('Programmer', 'New York', 'Rana', '2 Children')
