bc6932fee4cdc0980bedc0d927e3efe656f7d9a7
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-27 19:39:27 -0300

    Module of today 20.9.2023

---
+++ 9.1_loop_in_a_loop.js
@@ -0,0 +1,9 @@
+const listOfNeighbours = [ ["Canada", "Mexico"],
+    ["Spain"], ["Norway", "Sweden", "Russia"],
+    ];
+for(item of listOfNeighbours) {
+    for(word of item)
+    console.log(`Neighbour: ${word}`);
+}
+
+
