33eaedfdaff29f9c899540e6f3d874bec9f86901
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-27 14:46:46 -0300

    Module of today 20.9.2023

---
+++ 2.1_functions.js
@@ -0,0 +1,7 @@
+function countryInfo(country, population, capitalCity){
+    console.log(`${country} has ${population} and its capital city is ${capitalCity}`);
+}
+
+//countryInfo('Spain','47 million people','Madrid')
+//countryInfo('Israel','10 million people','jerusalem');
+countryInfo('USA', '300 million people', 'Woshington')
