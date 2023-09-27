203037bb98a85c32033025c4bab2964a73692a8c
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-09-27 15:54:32 -0300

    Module of today 20.9.2023

---
+++ 3.2_declaring_functions_1.js
@@ -0,0 +1,16 @@
+const welcome1 = () => console.log('Welcome to Appleseeds Bootcamp!');
+
+    const power=(a) => {let myNumber = a;
+    let result = Math.pow(myNumber, 2)
+    return result
+    };
+    
+    //welcome1()
+    //console.log(power(2))
+
+function squareRoot(a) {console.log(Math.sqrt(a))};
+function randomNumbers(a, b){console.log(Math.random() * (a - b) +
+b);}
+
+//squareRoot(9)
+//randomNumbers(3,4)
