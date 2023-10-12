214cf6e1672ffec375e82077f456929812b3202b
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-10-12 18:55:25 +0300

    Module of today 26.9.2023

---
+++ 15.1_get_sum.js
@@ -0,0 +1,20 @@
+let arr1 = [1,2,3];
+let arr2 = [5,66,23];
+
+function getSum(arr1, arr2){
+    let sum = 0;
+    for (let i=0; i < arr1.length; i++){
+    sum += arr1[i];
+    }
+    for (let i=0; i < arr2.length; i++){
+    sum += arr2[i];
+    }
+    return sum
+}
+console.log(getSum(arr1, arr2));
+
+// 1. arr1.length ///
+
+// 2. i use DEBUG CONSOLE///
+
+// 3. arr1 & arr2 arn`t defined/////
