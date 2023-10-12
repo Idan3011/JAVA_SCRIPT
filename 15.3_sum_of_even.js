9e35854639e22953882e913655fcb8c93a0e61ae
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-10-12 19:07:41 +0300

    Module of today 26.9.2023

---
+++ 15.3_sum_of_even.js
@@ -0,0 +1,13 @@
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+function getSumOfEven(arr){
+    
+    return arr[1] + arr[3] + arr[5] + arr[7] +
+    arr[9];
+    
+} 
+console.log(getSumOfEven(arr));
+
+// 1. the return index arent the even numbers & arr insnt declare///
+
+// 2. i use DEABUG CONSOL.E.///
