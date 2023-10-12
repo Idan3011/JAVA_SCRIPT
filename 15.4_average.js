e94031a52ab613c8b9b9d6422ba7468df264522d
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-10-12 19:13:13 +0300

    Module of today 26.9.2023

---
+++ 15.4_average.js
@@ -0,0 +1,13 @@
+arr = [ 85 , 90 , 92 ]
+
+function calcAverage (arr){
+    var sum =0 ;
+    for ( var i = 0 ; i < arr .length; i ++ ){
+    sum += arr [ i ];
+    } return sum ;
+}
+    console.log(calcAverage (arr));
+
+// 1. i use DEABUG CONSOLE.//
+
+//2. SUM ISNT DECLARE//
