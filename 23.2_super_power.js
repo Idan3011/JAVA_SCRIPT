664b33cacb80a155fce4996058403140b53bb0a8
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-11-13 11:22:08 +0200

    Module of today 13.11.2023

---
+++ 23.2_super_power.js
@@ -0,0 +1,13 @@
+const storm = {
+    superPower: 'flying',
+    callSuperPowerFunction: function printSuperPower() {
+        console.log("my superpower is " +
+        this.superPower);
+    } 
+}
+
+storm.callSuperPowerFunction()
+    
+
+
+
