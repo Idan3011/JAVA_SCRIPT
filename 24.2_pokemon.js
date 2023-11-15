49f4e2a7712c6f55bdb569c68ad7d948b6615206
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-11-15 12:54:51 +0200

    Module of today 15.11.2023

---
+++ 24.2_pokemon.js
@@ -0,0 +1,20 @@
+function Pokemon(pokemonName, pokemonType,
+    pokemonAttackList){
+    this.name = pokemonName;
+    this.type = pokemonType;
+    this.attackList = pokemonAttackList;
+}
+
+const pickacho = new Pokemon('Pickachu', 'elecricity', ['electirc', 'kick', 'jump' ]);
+const charmander = new Pokemon('Charmander', 'Fire', ['Fire', 'Kick', 'Jump' ]);
+const balbazur = new Pokemon('Balbazur', 'Water', ['Water Shoot', 'kick', 'jump' ]);
+Pokemon.prototype.callPokemon = function (){
+        console.log(`I choose you ${this.name}`)
+}
+// pickacho.callPokemon()
+
+Pokemon.prototype.atteck = function(n){
+    console.log(`${this.name} used ${this.attackList[n]}`);
+}
+
+charmander.atteck(0)
